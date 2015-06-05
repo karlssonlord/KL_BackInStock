842c599487bc7f1c611f8462f83cf5ba88726de9
David Wickström <davidwickstrom@gmail.com>
2015-06-05 11:42:09 +0200

    Add ability to generate secure subscription url

--- src/app/code/community/KL/BackInStock/Block/Modal.php
+++ src/app/code/community/KL/BackInStock/Block/Modal.php
@@ -2,8 +2,8 @@
 
 class KL_BackInStock_Block_Modal extends Mage_Catalog_Block_Product_View
 {
-    public function getSubscribeUrl()
+    public function getSubscribeUrl($secure = false)
     {
-        return Mage::getUrl('backinstock/notification/subscribe');
+        return Mage::getUrl('backinstock/notification/subscribe', array('_secure' => $secure));
     }
 }
